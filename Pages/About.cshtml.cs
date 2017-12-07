26a44b838be094c4cb4826b161d57ca1aea6da50
Rafael Alfaro <rafaela@thehangar.cr>
2017-12-07 08:48:36 -0600

    updates about message

--- Pages/About.cshtml.cs
+++ Pages/About.cshtml.cs
@@ -12,7 +12,7 @@ namespace aspnetcoreapp.Pages
 
         public void OnGet()
         {
-            Message = "Your application description page.";
+            Message = "This is an example project to learn ASP.net .";
         }
     }
 }
